164b507f904011e1b04e43000ca1328b95c62aa3
Umair <umairzahiraj16@gmail.com>
2025-01-29 18:34:09 +0530

    Basic backend structure created

---
+++ server/app.ts
@@ -0,0 +1,13 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+
+
+
+
+
+export default app;
