6bf680fdf95efd7c6240a0a4135cc7e625ba9c09
Umair <umairzahiraj16@gmail.com>
2025-01-31 21:16:39 +0530

    Registration method is defined, mail structure design complete

---
+++ server/controlers/userControler.ts
@@ -0,0 +1,55 @@
+import {Request,Response,NextFunction} from 'express'
+import userModel,{IUser} from '../models/user.model'
+import ErrorHandler from '../utils/ErrorHandler'
+import catchAsyncError  from '../middleware/catchAsyncErrors'
+import jwt,{Secret} from 'jsonwebtoken'
+import ejs from 'ejs'
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config()
+
+//Register User
+interface IUserRegisterBody{
+    name :string
+    email:string
+    password:string
+    avatar?:string
+}
+
+export const registerUser = catchAsyncError(async(req:Request,res:Response,next:NextFunction)=>{
+    try {
+        const {name,email,password} =req.body
+        const isEmailExists = await userModel.findOne({email})
+        if(isEmailExists){
+            return next(new ErrorHandler("Email already exists",400))
+        }
+        const user :IUserRegisterBody = {
+            name,
+            email,
+            password
+        }
+        const activationToken = createActivationToken(user)
+        const activationCode = activationToken.activationCode
+        const data = {user: {name:user.name},activationCode}
+        const html = ejs.renderFile(path.join(__dirname,'../mails/activation.mail.ejs'),data)
+
+    } catch (error) {
+        return  next(new ErrorHandler(error.message,400))
+    }
+})
+
+interface IActivationToken{
+    token:string
+    activationCode :string
+}
+
+function createActivationToken(user:any):IActivationToken{
+
+    const activationCode = Math.floor(1000+Math.random()*9000).toString()
+
+    const token = jwt.sign({user,activationCode}, //payload
+                        process.env.ACTIVATION_SECRET as Secret,//secret
+                        {expiresIn:'10m'}) // options
+
+    return {token,activationCode}
+}
