e8a219ef95f720d435e14a317f16529c5e95c38e
Umair <umairzahiraj16@gmail.com>
2025-01-31 11:57:41 +0530

    User schema is defined

---
+++ server/models/user.model.ts
@@ -0,0 +1,64 @@
+import mongoose,{Document,Model,Schema} from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const emailRegEx:RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document{
+    name : string
+    email : string
+    password : string
+    avatar : {
+        public_id : string
+        url : string
+    }
+    role :string
+    isVerified : boolean
+    courses :Array<{courseId : string}>
+    comparePassword : (password : string) => Promise<boolean>
+
+}
+
+const userSchema : Schema<IUser> = new mongoose.Schema({
+    name : {
+        type: String,
+        required : [true,'Please enter your name']
+    },
+    email : {
+        type:String,
+        required : [true,'Please enter your Email'],
+        validate : {
+            validator : function (value :string){
+                return emailRegEx.test(value)
+            },
+            message : "Please enter valid email"
+        },
+        unique : true
+    },
+    password : {
+        type : String,
+        required : [true,"Please provide your Email Address"],
+        minlength : [6,"Password must be at least 6 characters"],
+        select : false
+    },
+    avatar : {
+        public_id : String,
+        url :String
+    },
+    role : {
+        type : String,
+        default : "user"
+    },
+    isVerified : {
+        type : Boolean,
+        default : false
+    },
+    courses : [
+        {
+            courseId : String,
+
+        }
+    ]
+},{timestamps:true})
+
+
+// Hash password before storing
