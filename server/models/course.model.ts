56b762365b04bbfb4df8626e815eb15e391698ba
Umair <umairzahiraj16@gmail.com>
2025-02-16 23:07:49 +0530

    Course related schemas are created

--- server/models/course.model.ts
+++ server/models/course.model.ts
@@ -3,6 +3,7 @@ import mongoose,{Document,Model,Schema} from 'mongoose'
 interface IComment extends Document{
     user:object
     comment : string
+    commentReply? : IComment[]
 }
 
 interface IReview extends Document{
@@ -31,7 +32,7 @@ interface ICourseData extends Document{
 }
 
 interface ICourse extends Document{
-    title:string
+    name:string
     description : string
     price : number
     estimatedPrice : number
@@ -46,3 +47,93 @@ interface ICourse extends Document{
     courseData : ICourseData[]
     purchased? : number
 }
+
+
+const reviewSchema = new Schema<IReview>({
+    user : Object,
+    rating: {
+        type: Number,
+        default: 0
+    },
+    comment : String
+})
+
+const linkSchema = new Schema<ILink>({
+    title : String,
+    url : String
+})
+
+const commentSchema = new Schema<IComment>({
+    user : Object,
+    comment : String,
+    commentReply : [Object]
+})
+
+const courseDataSchema = new Schema<ICourseData>({
+    title : String,
+    description : String,
+    videoUrl : String,  
+    videoThumbnail : Object,
+    videoSection : String,
+    videoLength : String,
+    videoPlayer : String,
+    links : [linkSchema],
+    suggestion : String,
+    questions : [commentSchema]
+})
+
+const courseSchema = new Schema<ICourse>({
+    name : {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    estimatedPrice:{
+        type: Number
+    },
+    thumbnail : {
+        public_id : {
+            type: String,
+            required: true
+        },
+        url : {
+            type: String,
+            required: true
+        }
+    },
+    tags : {
+        type: String,
+        required: true
+    },
+    level : {
+        type: String,
+        required: true
+    },
+    demoUrl : {
+        type: String,
+        required: true
+    },
+    benefits : [{
+        title : String
+    }],
+    prerequisites : [{
+        title : String
+    }],
+    review : [reviewSchema],
+    courseData : [courseDataSchema],
+    rating : {
+        type: Number,
+        default: 0
+    },
+    purchased : {
+        type: Number,
+        default: 0
+    }
+})
