879947b8bcc4c58c0068eb206f48c6e4cb217dce
Umair <umairzahiraj16@gmail.com>
2025-02-22 00:12:08 +0530

    Edit course route working fine

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { uploadCourse } from '../controllers/course.controller'
+import { editCourse, uploadCourse } from '../controllers/course.controller'
 import {authorizeRoles, isAuthenticated} from '../middleware/auth'
 
 
@@ -10,6 +10,12 @@ courseRouter.post('/create-course',
     isAuthenticated,
     authorizeRoles("admin"),
     uploadCourse)
+
+courseRouter.put('/edit-course/:id', 
+    isAuthenticated, 
+    authorizeRoles("admin"),
+    editCourse);
+
 
 
 
